c50d0748afc4558a06b1200e804048637dd77590
Larry Liang <lian999111@gmail.com>
2019-12-06 00:13:22 +0100

    Use gpu:1 as well

--- test_tf1.py
+++ test_tf1.py
@@ -5,13 +5,13 @@ with tf.device('/cpu:0'):
     x = tf.placeholder(tf.float32, [None, 784])
 with tf.device('/gpu:0'):
     W = tf.Variable(tf.zeros([784, 10]))
-with tf.device('/gpu:0'):
+with tf.device('/gpu:1'):
     b = tf.Variable(tf.zeros([10]))
 with tf.device('/cpu:0'):
     y_1 = tf.matmul(x, W)
 with tf.device('/cpu:0'):
     y_2 = tf.add(y_1, b)
-with tf.device('/gpu:0'):
+with tf.device('/gpu:1'):
     y = tf.nn.softmax(y_2)
 
 with tf.Session() as sess:
